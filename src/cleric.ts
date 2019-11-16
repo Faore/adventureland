a54e33ccfb16ea279c5b248f01ee26a081deff56
Chris Thomas <faore@Christophers-MacBook-Pro.local>
2019-11-16 09:09:16 -0800

    Basic Priority Queue

--- src/cleric.ts
+++ src/cleric.ts
@@ -1,66 +1,10 @@
 import { Entity } from './definitions/adventureland'
-// Hey there!
-// This is CODE, lets you control your character with code.
-// If you don't know how to code, don't worry, It's easy.
-// Just set attack_mode to true and ENGAGE!
 
-var attack_mode=true
-
-
-game_log("Cleric Started Up!");
-
-
-setInterval(function(){
-	var target=get_targeted_monster();
-	
-	//if taking extra damage begin to move away (gets stuck easily)
-	//need to add tracker that if stuck then move randomly
-	if((character.hp < character.max_hp) && in_attack_range(target)){
-		move(
-			character.x+(-target.x+character.x),
-			character.y+(-target.y+character.y)
-			);//moves in opposite direction of attacker		
-	}
-	
-	
-	use_hp_or_mp();
-	loot();
-
-	if(!attack_mode || character.rip || is_moving(character)) return;
-
-	
-	if(!target)
-	{
-		//target=get_nearest_monster({min_xp:100,max_att:120});
-		if(target) change_target(target);
-		else
-		{
-			set_message("No Monsters");
-			return;
-		}
-	}
-	
-	if(!in_attack_range(target))
-	{
-		move(
-			character.x+(target.x-character.x)/2,
-			character.y+(target.y-character.y)/2
-			);
-		// Walk half the distance
-	}
-	else if(can_attack(target))
-	{
-		set_message("Attacking");
-		attack(target);
-	}
-	
-	
-	
-},1000/4); // Loops every 1/4 seconds.
+game_log("Cleric Loaded.");
 
 class Cleric {
     public run() {
-        game_log("Cleric Running");
+        game_log("Cleric Running.");
     }
 }
 
---
+++ src/lib/priority_queue.ts
@@ -0,0 +1,48 @@
+class PriorityQueue {
+
+    priorities: any[][];
+
+    constructor(priorities:number) {
+        this.priorities = [];
+
+        for(let i = 0; i < priorities; i++) {
+            this.priorities[i] = [];
+        }
+    }
+
+    public append(item: any, priority: number): void {
+        this.priorities[priority].push(item);
+    }
+    public prepend(item: any, priority: number): void {
+        this.priorities[priority].unshift(item);
+    }
+
+    public peek(): any {
+        let index = this.lowest_priority_with_items();
+        if (index === null) {
+            return null;
+        }
+
+        return this.priorities[index][0];
+    }
+    public pop(): any {
+        let index = this.lowest_priority_with_items();
+        if (index === null) {
+            return null;
+        }
+
+        return this.priorities[index].shift();
+    }
+
+    private lowest_priority_with_items(): number | null {
+        for (let i = 0; i < this.priorities.length; i++) {
+            if (this.priorities[i].length > 0) {
+                return i;
+            }
+        }
+        return null;
+    }
+
+}
+
+export {PriorityQueue};
